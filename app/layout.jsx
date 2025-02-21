639149946bb41bba662f2386c26be269a9ff7417
Ankush Kumar <imankush1010@gmail.com>
2025-02-21 17:26:14 +0530

    Completed

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,3 @@
-import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -19,9 +18,7 @@ export const metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body
